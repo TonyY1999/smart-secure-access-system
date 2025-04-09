bd2b0f391bbe6c147a0029453d386a2cc44138f2
yuezhang2003 <yuezy@seas.upenn.edu>
2025-04-09 14:03:55 -0400

    back clithread.c

--- Application/src/CliThread/CliThread.c
+++ Application/src/CliThread/CliThread.c
@@ -1,57 +1,34 @@
-/**************************************************************************/ /**
- * @file      CliThread.c
- * @brief     File for the CLI Thread handler. Uses FREERTOS + CLI
- * @author    Eduardo Garcia
- * @date      2020-02-15
+/**
+* @file      CliThread.c
+* @brief     File for the CLI Thread handler. Uses FREERTOS + CLI
+* @author    Eduardo Garcia
+* @date      2020-02-15
 
- ******************************************************************************/
+******************************************************************************/
 
 /******************************************************************************
  * Includes
  ******************************************************************************/
 #include "CliThread.h"
-#include "SerialConsole.h"
+
+#include "I2cDriver/I2cDriver.h"
+#include "WifiHandlerThread/WifiHandler.h"
 
 /******************************************************************************
  * Defines
  ******************************************************************************/
-#define FIRMWARE_VERSION "0.0.1"
 
 /******************************************************************************
  * Variables
  ******************************************************************************/
-static int8_t *const pcWelcomeMessage =
-    "FreeRTOS CLI.\r\nType Help to view a list of registered commands.\r\n";
+static const char pcWelcomeMessage[] = "FreeRTOS CLI.\r\nType Help to view a list of registered commands.\r\n";
 
-// Clear screen command
-const CLI_Command_Definition_t xClearScreen =
-    {
-        CLI_COMMAND_CLEAR_SCREEN,
-        CLI_HELP_CLEAR_SCREEN,
-        CLI_CALLBACK_CLEAR_SCREEN,
-        CLI_PARAMS_CLEAR_SCREEN};
+static const CLI_Command_Definition_t xOTAUCommand = {"fw", "fw: Download a file and perform an FW update\r\n", (const pdCOMMAND_LINE_CALLBACK)CLI_OTAU, 0};
+static const CLI_Command_Definition_t xResetCommand = {"reset", "reset: Resets the device\r\n", (const pdCOMMAND_LINE_CALLBACK)CLI_ResetDevice, 0};
+static const CLI_Command_Definition_t xI2cScan = {"i2c", "i2c: Scans I2C bus\r\n", (const pdCOMMAND_LINE_CALLBACK)CLI_i2cScan, 0};
+const CLI_Command_Definition_t xClearScreen = {CLI_COMMAND_CLEAR_SCREEN, CLI_HELP_CLEAR_SCREEN, CLI_CALLBACK_CLEAR_SCREEN, CLI_PARAMS_CLEAR_SCREEN};
 
-static const CLI_Command_Definition_t xResetCommand =
-    {
-        "reset",
-        "reset: Resets the device\r\n",
-        (const pdCOMMAND_LINE_CALLBACK)CLI_ResetDevice,
-        0};
-
-// add CLI commands	
-static const CLI_Command_Definition_t xVersionCommand =
-	{
-		"version",  
-		"version: Prints the firmware version.\r\n",  
-		(const pdCOMMAND_LINE_CALLBACK)CLI_Version, 
-		0};
-
-static const CLI_Command_Definition_t xTicksCommand =
-	{
-		"ticks",
-		TICK_CMD_HELP,
-		(const pdCOMMAND_LINE_CALLBACK)CLI_Ticks,
-		0};
+SemaphoreHandle_t cliCharReadySemaphore;  ///< Semaphore to indicate that a character has been received
 
 /******************************************************************************
  * Forward Declarations
@@ -67,14 +44,15 @@ static void FreeRTOS_read(char *character);
  ******************************************************************************/
 
 void vCommandConsoleTask(void *pvParameters)
-{	
+{
     // REGISTER COMMANDS HERE
+    FreeRTOS_CLIRegisterCommand(&xOTAUCommand);
     FreeRTOS_CLIRegisterCommand(&xClearScreen);
     FreeRTOS_CLIRegisterCommand(&xResetCommand);
-	FreeRTOS_CLIRegisterCommand(&xVersionCommand);
-	FreeRTOS_CLIRegisterCommand(&xTicksCommand);
+    FreeRTOS_CLIRegisterCommand(&xI2cScan);
 
-    uint8_t cRxedChar[2], cInputIndex = 0;
+    char cRxedChar[2];
+    unsigned char cInputIndex = 0;
     BaseType_t xMoreDataToFollow;
     /* The input and output buffers are declared static to keep them off the stack. */
     static char pcOutputString[MAX_OUTPUT_LENGTH_CLI], pcInputString[MAX_INPUT_LENGTH_CLI];
@@ -83,46 +61,44 @@ void vCommandConsoleTask(void *pvParameters)
     static char pcEscapeCodes[4];
     static uint8_t pcEscapeCodePos = 0;
 
-    // Any semaphores/mutexes/etc you needed to be initialized, you can do them here
-
     /* This code assumes the peripheral being used as the console has already
     been opened and configured, and is passed into the task as the task
     parameter.  Cast the task parameter to the correct type. */
 
     /* Send a welcome message to the user knows they are connected. */
-    SerialConsoleWriteString(pcWelcomeMessage);
-    char rxChar;
-    for (;;)
-    {
-        /* This implementation reads a single character at a time.  Wait in the
-        Blocked state until a character is received. */
-        FreeRTOS_read(&cRxedChar);
-		
-        if (cRxedChar[0] == '\n' || cRxedChar[0] == '\r')
-        {
+    SerialConsoleWriteString((char *)pcWelcomeMessage);
+
+    // Any semaphores/mutexes/etc you needed to be initialized, you can do them here
+    cliCharReadySemaphore = xSemaphoreCreateBinary();
+    if (cliCharReadySemaphore == NULL) {
+        LogMessage(LOG_ERROR_LVL, "Could not allocate semaphore\r\n");
+        vTaskSuspend(NULL);
+    }
+
+    for (;;) {
+        FreeRTOS_read(&cRxedChar[0]);
+
+        if (cRxedChar[0] == '\n' || cRxedChar[0] == '\r') {
             /* A newline character was received, so the input command string is
             complete and can be processed.  Transmit a line separator, just to
             make the output easier to read. */
-            SerialConsoleWriteString("\r\n");
-			
+            SerialConsoleWriteString((char *)"\r\n");
             // Copy for last command
             isEscapeCode = false;
             pcEscapeCodePos = 0;
             strncpy(pcLastCommand, pcInputString, MAX_INPUT_LENGTH_CLI - 1);
-            pcLastCommand[MAX_INPUT_LENGTH_CLI - 1] = 0; // Ensure null termination
+            pcLastCommand[MAX_INPUT_LENGTH_CLI - 1] = 0;  // Ensure null termination
 
             /* The command interpreter is called repeatedly until it returns
             pdFALSE.  See the "Implementing a command" documentation for an
             explanation of why this is. */
-            do
-            {
+            do {
                 /* Send the command string to the command interpreter.  Any
                 output generated by the command interpreter will be placed in the
                 pcOutputString buffer. */
-                xMoreDataToFollow = FreeRTOS_CLIProcessCommand(
-                    pcInputString,        /* The command string.*/
-                    pcOutputString,       /* The output buffer. */
-                    MAX_OUTPUT_LENGTH_CLI /* The size of the output buffer. */
+                xMoreDataToFollow = FreeRTOS_CLIProcessCommand(pcInputString,        /* The command string.*/
+                                                               pcOutputString,       /* The output buffer. */
+                                                               MAX_OUTPUT_LENGTH_CLI /* The size of the output buffer. */
                 );
 
                 /* Write the output generated by the command interpreter to the
@@ -138,35 +114,26 @@ void vCommandConsoleTask(void *pvParameters)
             to receive the next command. */
             cInputIndex = 0;
             memset(pcInputString, 0x00, MAX_INPUT_LENGTH_CLI);
-        }
-        else
-        {
+            memset(pcOutputString, 0, MAX_OUTPUT_LENGTH_CLI);
+        } else {
             /* The if() clause performs the processing after a newline character
-    is received.  This else clause performs the processing if any other
-    character is received. */
+is received.  This else clause performs the processing if any other
+character is received. */
 
-            if (true == isEscapeCode)
-            {
-
-                if (pcEscapeCodePos < CLI_PC_ESCAPE_CODE_SIZE)
-                {
+            if (true == isEscapeCode) {
+                if (pcEscapeCodePos < CLI_PC_ESCAPE_CODE_SIZE) {
                     pcEscapeCodes[pcEscapeCodePos++] = cRxedChar[0];
-                }
-                else
-                {
+                } else {
                     isEscapeCode = false;
                     pcEscapeCodePos = 0;
                 }
 
-                if (pcEscapeCodePos >= CLI_PC_MIN_ESCAPE_CODE_SIZE)
-                {
-
+                if (pcEscapeCodePos >= CLI_PC_MIN_ESCAPE_CODE_SIZE) {
                     // UP ARROW SHOW LAST COMMAND
-                    if (strcasecmp(pcEscapeCodes, "oa"))
-                    {
+                    if (strcasecmp(pcEscapeCodes, "oa")) {
                         /// Delete current line and add prompt (">")
                         sprintf(pcInputString, "%c[2K\r>", 27);
-                        SerialConsoleWriteString(pcInputString);
+                        SerialConsoleWriteString((char *)pcInputString);
                         /// Clear input buffer
                         cInputIndex = 0;
                         memset(pcInputString, 0x00, MAX_INPUT_LENGTH_CLI);
@@ -184,36 +151,28 @@ void vCommandConsoleTask(void *pvParameters)
             is received.  This else clause performs the processing if any other
             character is received. */
 
-            else if (cRxedChar[0] == '\r')
-            {
+            else if (cRxedChar[0] == '\r') {
                 /* Ignore carriage returns. */
-            }
-            else if (cRxedChar[0] == ASCII_BACKSPACE || cRxedChar[0] == ASCII_DELETE)
-            {
+            } else if (cRxedChar[0] == ASCII_BACKSPACE || cRxedChar[0] == ASCII_DELETE) {
                 char erase[4] = {0x08, 0x20, 0x08, 0x00};
                 SerialConsoleWriteString(erase);
                 /* Backspace was pressed.  Erase the last character in the input
                 buffer - if there are any. */
-                if (cInputIndex > 0)
-                {
+                if (cInputIndex > 0) {
                     cInputIndex--;
                     pcInputString[cInputIndex] = 0;
                 }
             }
             // ESC
-            else if (cRxedChar[0] == ASCII_ESC)
-            {
-                isEscapeCode = true; // Next characters will be code arguments
+            else if (cRxedChar[0] == ASCII_ESC) {
+                isEscapeCode = true;  // Next characters will be code arguments
                 pcEscapeCodePos = 0;
-            }
-            else
-            {
+            } else {
                 /* A character was entered.  It was not a new line, backspace
                 or carriage return, so it is accepted as part of the input and
                 placed into the input buffer.  When a n is entered the complete
                 string will be passed to the command interpreter. */
-                if (cInputIndex < MAX_INPUT_LENGTH_CLI)
-                {
+                if (cInputIndex < MAX_INPUT_LENGTH_CLI) {
                     pcInputString[cInputIndex] = cRxedChar[0];
                     cInputIndex++;
                 }
@@ -226,29 +185,30 @@ void vCommandConsoleTask(void *pvParameters)
     }
 }
 
-/**************************************************************************/ /**
- * @fn			void FreeRTOS_read(char* character)
- * @brief		This function block the thread unless we received a character.
-                 There are multiple solutions! Check all the inter-thread communications available! See https://www.freertos.org/a00113.html
- * @details		STUDENTS TO COMPLETE.
- * @note
- *****************************************************************************/
+/**
+* @fn			void FreeRTOS_read(char* character)
+* @brief		STUDENTS TO COMPLETE. This function block the thread unless we received a character. How can we do this?
+                                There are multiple solutions! Check all the inter-thread communications available! See https://www.freertos.org/a00113.html
+* @details		STUDENTS TO COMPLETE.
+* @note
+*****************************************************************************/
 static void FreeRTOS_read(char *character)
 {
-	SemaphoreHandle_t sem = GetSerialRxSemaphore();
-	
-	if (character == NULL) {
-		return;
-	}
+    vTaskSuspend(NULL);  // We suspend ourselves. Please remove this when doing your code
+}
 
-	// wait for data
-	if (xSemaphoreTake(sem, portMAX_DELAY) == pdTRUE)
-	{		
-		if (SerialConsoleReadCharacter((uint8_t*)character) == -1)
-		{
-			*character = '\0';
-		}
-	}
+/**
+ * @fn			void CliCharReadySemaphoreGiveFromISR(void)
+ * @brief		Give cliCharReadySemaphore binary semaphore from an ISR
+ * @details
+ * @note
+ */
+void CliCharReadySemaphoreGiveFromISR(void)
+{
+    static BaseType_t xHigherPriorityTaskWoken;
+    xHigherPriorityTaskWoken = pdFALSE;
+    xSemaphoreGiveFromISR(cliCharReadySemaphore, &xHigherPriorityTaskWoken);
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
 /******************************************************************************
@@ -267,35 +227,61 @@ BaseType_t xCliClearTerminalScreen(char *pcWriteBuffer, size_t xWriteBufferLen, 
     return pdFALSE;
 }
 
-// Example CLI Command. Resets system. 
+// Example CLI Command. Reads from the IMU and returns data.
+BaseType_t CLI_OTAU(int8_t *pcWriteBuffer, size_t xWriteBufferLen, const int8_t *pcCommandString)
+{
+    WifiHandlerSetState(WIFI_DOWNLOAD_INIT);
+
+    return pdFALSE;
+}
+
+// Example CLI Command. Resets system.
 BaseType_t CLI_ResetDevice(int8_t *pcWriteBuffer, size_t xWriteBufferLen, const int8_t *pcCommandString)
 {
-	vTaskDelay(pdMS_TO_TICKS(30));
     system_reset();
     return pdFALSE;
 }
 
-BaseType_t CLI_Version(char *pcWriteBuffer,size_t xWriteBufferLen,const int8_t *pcCommandString)
+/**
+ * @brief    Scans fot connected i2c devices
+ * @param    p_cli
+ * @param    argc
+ * @param    argv
+ ******************************************************************************/
+BaseType_t CLI_i2cScan(int8_t *pcWriteBuffer, size_t xWriteBufferLen, const int8_t *pcCommandString)
 {
-	if (pcWriteBuffer == NULL) {
-		return pdFALSE;
-	}
-	snprintf(pcWriteBuffer, xWriteBufferLen, "Firmware Version: %s\r\n", FIRMWARE_VERSION);
-	return pdFALSE;
-}
+    I2C_Data i2cDevice;
+    uint8_t address;
+    // Send 0 command byte
+    uint8_t dataOut[2] = {0, 0};
+    uint8_t dataIn[2];
+    dataOut[0] = 0;
+    dataOut[1] = 0;
+    i2cDevice.address = 0;
+    i2cDevice.msgIn = (uint8_t *)&dataIn[0];
+    i2cDevice.lenOut = 1;
+    i2cDevice.msgOut = (const uint8_t *)&dataOut[0];
+    i2cDevice.lenIn = 1;
 
-BaseType_t CLI_Ticks(char *pcWriteBuffer,size_t xWriteBufferLen,const int8_t *pcCommandString)
-{
-	TickType_t tickCount;
-	if (xTaskGetSchedulerState() == taskSCHEDULER_NOT_STARTED)
-	{
-		snprintf(pcWriteBuffer, xWriteBufferLen, "Scheduler not started yet.\r\n");
-	}
-	else
-	{
-		tickCount = xTaskGetTickCount();
-		snprintf(pcWriteBuffer, xWriteBufferLen, "Ticks since start: %u\r\n", (unsigned long)tickCount);
-	}
+    SerialConsoleWriteString("0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\r\n");
+    for (int i = 0; i < 128; i += 16) {
+        snprintf(bufCli, CLI_MSG_LEN - 1, "%02x: ", i);
+        SerialConsoleWriteString(bufCli);
 
-	return pdFALSE;
+        for (int j = 0; j < 16; j++) {
+            i2cDevice.address = (i + j) << 1;
+
+            int32_t ret = I2cWriteDataWait(&i2cDevice, 100);
+            if (ret == 0) {
+                snprintf(bufCli, CLI_MSG_LEN - 1, "%02x: ", i2cDevice.address);
+                SerialConsoleWriteString(bufCli);
+            } else {
+                snprintf(bufCli, CLI_MSG_LEN - 1, "X ");
+                SerialConsoleWriteString(bufCli);
+            }
+        }
+        SerialConsoleWriteString("\r\n");
+    }
+    SerialConsoleWriteString("\r\n");
+    return pdFALSE;
 }
