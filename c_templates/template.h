f0bb8e1204140d01f1dc293281adbe3f7c2f7bac
TonyY1999 <tonyyan909@gmail.com>
2025-04-18 23:22:46 -0400

    format c templates

---
+++ c_templates/template.h
@@ -0,0 +1,45 @@
+/**************************************************************************//**
+ * @file      template.h
+ * @brief     Template for ESE516 with Doxygen-style comments
+ * @author    Eduardo Garcia
+ * @date      2020-01-01
+ ******************************************************************************/
+
+#ifndef TEMP_H
+#define TEMP_H 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************
+ * Includes
+ ******************************************************************************/
+
+/******************************************************************************
+ * Defines
+ ******************************************************************************/
+
+/******************************************************************************
+ * Structures and Enumerations
+ ******************************************************************************/
+
+/******************************************************************************
+ * Global Function Declaration
+ ******************************************************************************/
+/**
+ * @fn		int ExampleFuncionComment(int inputInt, void *pvParameters)
+ * @brief	An example function comment. Erase me!
+ * @details 	Write details of function here.
+
+ * @param[in]	InputInt Use me to describe inputs to functions
+ * @param[out]	*pvParameters Use me to describe outputs of functions passed as arguments
+ * @return		Use me to explain the return of an argument.
+ * @note
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  //TEMP_H
